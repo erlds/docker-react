cebc32c117d01bf481259ebc1b8aaa31a907634c
Evaristo Ramalho <evaristo@seat.ind.br>
2021-09-27 11:22:07 -0300

    Mudando texto do app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          eae men
+          eae men, na nova branch agora
         </p>
         <a
           className="App-link"
